0f1ff36278caf20047cd03fb218a7a496bce8ff7
Elexy <alex@seretalabs.com>
2019-02-22 16:04:22 +0100

    move path to initial command and change path order

--- scripts/run.sh
+++ scripts/run.sh
@@ -16,11 +16,13 @@ You can use them in the follwoing fashion:
 
   docker run nearform/noise-cli:0.0.2 terraform
 
-You use all commands directly by executing this in a shell:
+You can use all commands directly by executing this in a shell:
 
   mkdir -p ~/cloud-cli && \
   docker run --rm -d --name cloud-cli -v ~/cloud-cli:/src/scripts_shared nearform/noise-cli:0.0.2 /src/scripts/keepalive && \
-  source ~/cloud-cli/setup.sh
+  export PATH=~/cloud-cli/:$PATH
+
+This will add all the tools to your path
 
 To stop the comtainer, run:
 
